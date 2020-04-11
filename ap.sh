ace80d2fe4eb88383b862b543268fa9f6dd16210
SuSux13 <55140831+SuSux13@users.noreply.github.com>
2020-04-11 20:37:12 +0200

    Create ap.sh

---
+++ ap.sh
@@ -0,0 +1,28 @@
+apt install ruby-dev
+#sudo apt-get install ruby-full
+apt install python
+apt install python2
+apt install python3
+apt install nano
+apt install apache2
+apachectl start
+#sudo emerge dev-lang/ruby
+pkg install runtime/ruby-18
+#sudo apt install autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm5 libgdbm-dev
+apt install autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm5 libgdbm-dev
+gem install pg
+apt install postgresql-server-dev-all
+#sudo apt-get install libpq-dev
+apt-get install libpq-dev
+#sudo apt-get install libpq-dev build-essential
+apt-get install ruby-dev
+#sudo apt-get install libmysqlclient15-dev -y
+#sudo gem install mysql
+#sudo apt-get install rubygems
+wget http://production.cf.rubygems.org/rubygems/rubygems-1.5.0.tgz
+tar xvf rubygems-1.5.0.tgz
+cd rubygems-1.5.0
+#sudo ruby setup.rb
+apt install gcc zlib zlib-devel
+apt-get install clang
+
